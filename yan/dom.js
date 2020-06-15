df965349a0060630c601d02a656534633baa9d6f
rwbeast <64768386+rwbeast@users.noreply.github.com>
2020-06-15 15:07:17 +0530

    Update dom.js
    added onready function

--- yan/dom.js
+++ yan/dom.js
@@ -6,6 +6,20 @@ export const qsa = (sel, ctx = doc) => ctx.querySelectorAll(sel);
 
 export const style = (el, obj) => Object.assign(el.style, obj);
 
-export const on = (el, evt, hand) => el.addEventListener(evt, hand);
+export const on = (el, evt, hand) => el.addEventListener(evt, hand, false);
 
-export const off = (el, evt, hand) => el.removeEventListener(evt, hand);
+export const off = (el, evt, hand) => el.removeEventListener(evt, hand, false);
+
+export const ready = hand => {
+
+  if (/complete|loaded|interactive/.test(doc.readyState) && doc.body) {
+
+    setTimeout(hand, 1);
+
+  } else {
+
+    on(doc, 'DOMContentLoaded', hand);
+
+  }
+
+};
