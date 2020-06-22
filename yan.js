22f0ed35aff4d44bdf517cd68f687aac8ca4a284
rwbeast <64768386+rwbeast@users.noreply.github.com>
2020-06-22 14:04:26 +0530

    Create yan.js

---
+++ yan.js
@@ -0,0 +1,10 @@
+
+/* yan.js - Minimal ES6 Toolkit For 2D Games */
+
+export const version = '0.1-alpha';
+
+export * as device from './yan/device.js';
+export * as math from './yan/math.js';
+export * as collision from './yan/collision.js';
+export * as dom from './yan/dom.js';
+export * as utils from './yan/utils.js';
