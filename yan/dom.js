ab15e3b3f04050b16341c808ef9bf94ff4d7eecf
rwbeast <64768386+rwbeast@users.noreply.github.com>
2020-06-22 14:14:13 +0530

    added attr function

--- yan/dom.js
+++ yan/dom.js
@@ -6,6 +6,18 @@ export const qsa = (sel, ctx = doc) => ctx.querySelectorAll(sel);
 
 export const style = (el, obj) => Object.assign(el.style, obj);
 
+export const attr = (el, name, val) => {
+
+  if(val) {
+ 
+    return el.setAttribute(name, val);
+ 
+  }
+
+  return el.getAttribute(name);
+
+};
+
 export const on = (el, evt, hand) => el.addEventListener(evt, hand, false);
 
 export const off = (el, evt, hand) => el.removeEventListener(evt, hand, false);
