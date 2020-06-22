7217676bd12765e73625e32000309e3b9008bc8d
rwbeast <64768386+rwbeast@users.noreply.github.com>
2020-06-22 14:02:50 +0530

    Delete yan.js

--- yan/yan.js
+++
@@ -1,9 +0,0 @@
-
-/* yan.js - minimal es6 game engine */
-
-export const version = '0.1-alpha';
-
-export * as device from './device.js';
-export * as math from './math.js';
-export * as collision from './collision.js';
-export * as dom from './dom.js';
