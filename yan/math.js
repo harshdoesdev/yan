5226a05e88887901690161aed75e15e44ca19292
rwbeast <64768386+rwbeast@users.noreply.github.com>
2020-06-22 14:02:07 +0530

    Update math.js

--- yan/math.js
+++ yan/math.js
@@ -1,8 +1,6 @@
 
 /* math.js */
 
-export const clamp = (num, min, max) => Math.max(min, Math.min(num, max));
-
 export const distance = (x1, y1, x2, y2) => {
 
   const dx = x1 - x2,
@@ -10,17 +8,5 @@ export const distance = (x1, y1, x2, y2) => {
         dy = y1 - y2;
 
   return Math.sqrt(dx * dx + dy * dy);
-
-};
-
-export const random = (min = 0, max = 1) => Math.random() * (max - min) + min;
-
-export const randomInt = (min = 0, max = 1) => {
-
-  min = Math.ceil(min);
-
-  max = Math.floor(max);
-
-  return Math.floor(Math.random() * (max - min + 1)) + min;
 
 };
